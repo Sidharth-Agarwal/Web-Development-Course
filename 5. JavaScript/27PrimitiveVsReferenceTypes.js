8481e3c8f4a23c1ea1712c09cc8eab3767d89ffd
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-12 13:10:15 +0530

    Primitive vs Reference Types

---
+++ 5. JavaScript/27PrimitiveVsReferenceTypes.js
@@ -0,0 +1,17 @@
+// Primitive Types
+let a = 10;
+let b = a;
+
+a++;
+
+console.log(a);
+console.log(b);
+
+// Reference Types
+let c = {value:10};
+let d = c;
+
+c.value++;
+
+console.log(c.value);
+console.log(d.value);
