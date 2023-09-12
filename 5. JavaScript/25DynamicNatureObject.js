f646f8a49eccf4202b03782d8241a30c443421e4
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-12 12:11:10 +0530

    Dynamic Nature of Objects

--- 5. JavaScript/25DynamicNatureObject.js
+++ 5. JavaScript/25DynamicNatureObject.js
@@ -1,11 +1,11 @@
-function CreateRectangle(){
-    this.length = 1;
-    this.breadth = 2;
+function CreateRectangle(len,bre){
+    this.length = len;
+    this.breadth = bre;
     this.draw = function(){
         console.log('Drawing rectangle...!');
     }
 }
 
-let rectangleObject = new CreateRectangle();
+let rectangleObject = new Rectangle(4,5);
 rectangleObject.color = 'yellow';
 delete rectangleObject.color;
