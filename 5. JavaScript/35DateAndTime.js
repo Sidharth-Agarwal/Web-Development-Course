b76206d3a828693a26098d85ab3707ef960b51ef
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-13 15:45:00 +0530

    Date and Time

---
+++ 5. JavaScript/35DateAndTime.js
@@ -0,0 +1,12 @@
+let date = new Date();
+
+let date2 = new Date('June 20 1998 07:15');
+
+let date3 = new Date(1998, 6, 20, 7);
+
+console.log(date);
+console.log(date2);
+console.log(date3);
+
+date3.setFullYear(1990);
+console.log(date3);
