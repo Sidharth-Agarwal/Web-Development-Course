ba3b73429d33c920ba2690c2763d79fc6ec78066
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-13 15:24:41 +0530

    String Literals

---
+++ 5. JavaScript/34StringLiteral.js
@@ -0,0 +1,25 @@
+let firstName = 'Sidharth';
+let lastName = 'Agarwal';
+
+let message = 'This is \n my first \n message';
+
+console.log(message);
+
+// Template Literals
+let message2 = `This is
+my first
+message
+`;
+
+console.log(message2);
+
+// Making it dynamic
+let message3 = `Hello ${firstName}
+
+Thank for the opportunity
+
+Regards
+${lastName}
+`
+
+console.log(message3);
