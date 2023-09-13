c2554f312392b68f5a0fbec45c67b4ee925b3e5d
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-13 17:37:15 +0530

    Inserting

--- 5. JavaScript/36Arrays.js
+++ 5. JavaScript/36Arrays.js
@@ -1,3 +1,11 @@
 let numbers = [1,4,5,7];
+console.log(numbers);
 
+numbers.push(10);
+console.log(numbers);
+
+numbers.unshift(-1);
+console.log(numbers);
+
+numbers.splice(2, 0, 'a','b','c','d');
 console.log(numbers);
