65eb2ee9c130be7b85b06b819eb6f06198346c25
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-14 10:04:53 +0530

    Arrays

--- 5. JavaScript/36Arrays.js
+++ 5. JavaScript/36Arrays.js
@@ -1,3 +1,5 @@
+// This was for the example of primitives
+
 let numbers = [1,4,5,7];
 console.log(numbers);
 
@@ -12,3 +14,13 @@ console.log(numbers);
 // Inserting in the middle
 numbers.splice(2, 0, 'a','b','c','d');
 console.log(numbers);
+
+// Searching in the array
+console.log(numbers.indexOf(4));
+// This is not a good practice
+
+// This is a good practice
+console.log(numbers.includes(5));
+
+// This is advanced search
+console.log(numbers.indexOf(4,8));
