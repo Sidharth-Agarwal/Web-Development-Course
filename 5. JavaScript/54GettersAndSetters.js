3354794cb3f975a37924d98fab8afb0d5da09800
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-16 13:03:40 +0530

    Getter and Setter

---
+++ 5. JavaScript/54GettersAndSetters.js
@@ -0,0 +1,25 @@
+let person = {
+    fName : 'Sidharth',
+    lName : 'Agarwal',
+    
+    get fullName(){
+        return `${person.fName} ${person.lName}`;
+    },
+
+    set fullName(value){
+        let parts = value.split(' ');
+        this.fName = parts[0];
+        this.lName = parts[1];
+    }
+};
+
+// function fullName(){
+//     return `${person.fName} ${person.lName}`;
+// }
+
+console.log(person.fullName);
+
+// Getter and Setter
+// getter -> access properties
+// setter -> change or mutate properties
+
