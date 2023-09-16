6316654e9206eadd9e53b5fb74420d1cb0a9b1a8
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-16 13:13:28 +0530

    Exception Handling

---
+++ 5. JavaScript/55ExceptionHandling.js
@@ -0,0 +1,30 @@
+// Getter and Setter
+// getter -> access properties
+// setter -> change or mutate properties
+
+let person = {
+    fName : 'Sidharth',
+    lName : 'Agarwal',
+    
+    get fullName(){
+        return `${person.fName} ${person.lName}`;
+    },
+
+    set fullName(value){
+        if(typeof value !== String){
+            throw new Error("You have not sent a string");
+        }
+        let parts = value.split(' ');
+        this.fName = parts[0];
+        this.lName = parts[1];
+    }
+};
+
+try{
+    person.fullName = 1;
+}
+catch(e){
+    alert(e);
+}
+
+console.log(person.fullName);
