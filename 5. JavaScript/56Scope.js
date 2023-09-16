b71b3a47452bd2bd039f0e5ade310790eca7dddc
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-09-16 13:20:57 +0530

    Scope

---
+++ 5. JavaScript/56Scope.js
@@ -0,0 +1,32 @@
+{
+    var a = 10
+    let b = 5;
+    // console.log(a);
+    // console.log(b);
+}
+
+console.log(a);
+// console.log(b);
+
+function walk(){
+    var c = 15;
+}
+
+// console.log(c);
+
+for(var i=0;i<10;i++){
+    // This will not work in case of let
+}
+
+console.log(i);
+// Same is the case with if condition
+
+function f1(){
+    const d = 100;
+}
+
+const e = 500;
+
+function f2(){
+    const f = 1000;
+}
